94cc5e7f595bc60f8c747f7e9cb6e99791abe2fb
Sheldon <ratchubadasratchufly@gmail.com>
2025-07-31 18:48:50 -0300

    inclusao do swagger

--- back-internet-banking/src/main/java/br/edu/ifba/internetbanking/demo/controllers/UserController.java
+++ back-internet-banking/src/main/java/br/edu/ifba/internetbanking/demo/controllers/UserController.java
@@ -1,12 +1,18 @@
 package br.edu.ifba.internetbanking.demo.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.edu.ifba.internetbanking.demo.entities.User;
 import br.edu.ifba.internetbanking.demo.inputdto.user.UserInputDTO;
 import br.edu.ifba.internetbanking.demo.outputdto.user.UserOutputDTO;
 import br.edu.ifba.internetbanking.demo.services.UserServices;
 
+@Controller
+@RequestMapping("/api/user")
 public class UserController {
 
     private UserServices userServices;
@@ -15,7 +21,8 @@ public class UserController {
         this.userServices = userServices;
     }
 
-    
+    @PostMapping
+    @Operation(summary = "Returns a http.OK + usuario-criado ou badRequest")
     public ResponseEntity<UserOutputDTO> createUser(UserInputDTO userInputDTO) {
        UserOutputDTO createdUser = this.userServices.createUser(userInputDTO);
 
